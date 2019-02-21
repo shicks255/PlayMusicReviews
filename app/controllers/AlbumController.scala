4b3edeeb4705ba9d5509781b4647411bae4c6d5f
shicks <shicks255@yahoo.com>
2019-02-20 22:13:39 -0500

    adding pre wraps to review content, and displaying the username next to the review

--- app/controllers/AlbumController.scala
+++ app/controllers/AlbumController.scala
@@ -4,7 +4,7 @@ import java.time.LocalDateTime
 
 import com.google.inject.{Inject, Singleton}
 import models.album.{Album, AlbumDao, AlbumFull}
-import models.review.{Review, ReviewDao}
+import models.review.{Review, ReviewDao, ReviewFull}
 import play.api.data.Form
 import play.api.data.Forms.{longNumber, mapping, _}
 import play.api.i18n.I18nSupport
@@ -26,20 +26,22 @@ class AlbumController @Inject() (cc: ControllerComponents, albumDao: AlbumDao, r
     val album: Album = albumDao.getAlbum(id).get
     val fullAlbum: AlbumFull = albumDao.getFullAlbum(album)
     val reviews: List[Review] = reviewDao.getAllReviews(id)
-    Ok(views.html.albumHome(reviews, fullAlbum, reviewForm, ""))
+    val fullReviews: List[Option[ReviewFull]] = reviews.map(reviewDao.getFullReview(_))
+    Ok(views.html.albumHome(fullReviews, fullAlbum, reviewForm, ""))
   }
 
   def addReview(albumId: Long) = Action { implicit request =>
     val album: Album = albumDao.getAlbum(albumId).get
     val fullAlbum: AlbumFull = albumDao.getFullAlbum(album)
     val reviews: List[Review] = reviewDao.getAllReviews(albumId)
+    val fullReviews: List[Option[ReviewFull]] = reviews.map(reviewDao.getFullReview(_))
     reviewForm.bindFromRequest().fold(
-      errors => (BadRequest(views.html.albumHome(reviews, fullAlbum, reviewForm, ""))),
+      errors => (BadRequest(views.html.albumHome(fullReviews, fullAlbum, reviewForm, ""))),
       form => {
         val newlyAddedId = reviewDao.saveReview(form)
         newlyAddedId match {
           case Some(x) => Redirect(routes.AlbumController.albumHome(form.albumId))
-          case None => Ok(views.html.albumHome(reviews, fullAlbum, reviewForm, ""))
+          case None => Ok(views.html.albumHome(fullReviews, fullAlbum, reviewForm, ""))
         }
       }
     )
