9622bbe0839c961dfcb7b644e27ddaeef1dcb6a1
shicks <shicks255@yahoo.com>
2019-02-25 21:40:56 -0500

    fixing the link and summary/bio part

--- app/models/artist/Artist.scala
+++ app/models/artist/Artist.scala
@@ -6,12 +6,20 @@ case class Artist(id: Option[Long],
                   summary: String,
                   content: String) {
 
-  def getSummaryWithoutLink = {
+  def getSummary = {
+    val start = summary.indexOf("<a href")
+    val end = summary.indexOf("</a>")
 
+    val link = summary.substring(start, end+4)
+    summary.replace(link, "")
   }
 
-  def getSummaryLink = {
+  def getLink = {
+    val start = summary.indexOf("<a href=")
+    val end = summary.indexOf(">", start)
 
+    val link = summary.substring(start+9, end-1)
+    link
   }
 
 }
--- app/models/artist/ArtistFull.scala
+++ app/models/artist/ArtistFull.scala
@@ -3,4 +3,22 @@ package models.artist
 import models.ImageGetter
 import models.artistImage.ArtistImage
 
-case class ArtistFull(id: Long, name: String, mbid: String, summary: String, content: String, images: List[ArtistImage]) extends ImageGetter
+case class ArtistFull(id: Long, name: String, mbid: String, summary: String, content: String, images: List[ArtistImage]) extends ImageGetter {
+
+  def getSummary = {
+    val start = summary.indexOf("<a href")
+    val end = summary.indexOf("</a>")
+
+    val link = summary.substring(start, end+4)
+    summary.replace(link, "")
+  }
+
+  def getLink = {
+    val start = summary.indexOf("<a href=")
+    val end = summary.indexOf(">", start)
+
+    val link = summary.substring(start+9, end-1)
+    link
+  }
+
+}
